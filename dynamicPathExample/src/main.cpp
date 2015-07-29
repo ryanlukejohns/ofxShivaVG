5b10c2dc695c2a5cb1fe80803ca23b022c170752
Constantine Tarasenkov <constantine@protonmail.com>
2015-07-30 02:19:41 +0300

    Update another example

--- dynamicPathExample/src/main.cpp
+++ dynamicPathExample/src/main.cpp
@@ -1,12 +1,13 @@
 #include "ofApp.h"
-#include "ofAppGLFWWindow.h"
 
 //--------------------------------------------------------------
 int main()
 {
-    ofPtr<ofAppGLFWWindow> win = ofPtr<ofAppGLFWWindow>(new ofAppGLFWWindow());
-    win->setStencilBits(8);
-	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(win, 1440, 1024, OF_WINDOW);
-	ofRunApp(new ofApp()); // start the app
+  ofGLFWWindowSettings settings;
+
+  auto window = ofCreateWindow(settings);
+  auto app = make_shared<ofApp>();
+  ofRunApp(window, app);
+
+  return ofRunMainLoop();
 }
